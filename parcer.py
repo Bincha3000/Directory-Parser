a5d00ef3e9b53ae2e1b05920288f8d490ba50085
Lancelot <bincha.1997@gmail.com>
2019-05-13 13:05:14 +1000

    Update

--- parcer.py
+++ parcer.py
@@ -1,9 +1,15 @@
+"""Checking folders for Python files and searching for English verbs"""
+
 import ast
 import os
 import collections
 import nltk
+
 from nltk import pos_tag
+
 nltk.download('averaged_perceptron_tagger')
+
+Path = ''
 
 def flat(_list):
     """ [(1,2), (3,4)] -> [1, 2, 3, 4]"""
@@ -16,19 +22,18 @@ def is_verb(word):
     pos_info = pos_tag([word])
     return pos_info[0][1] == 'VB'
 
-Path = ''
-
-def get_trees(_path, with_filenames=False, with_file_content=False):
+def check_py_files(path, max_len):
     filenames = []
-    trees = []
-    path= Path
     for dirname, dirs, files in os.walk(path, topdown=True):
         for file in files:
-            if file.endswith('.py'):
+            if file.endswith('.py') and len(filenames) <= 100:
                 filenames.append(os.path.join(dirname, file))
-                if len(filenames) == 100:
-                    break
     print('total %s files' % len(filenames))
+    return filenames
+
+def get_trees(with_filenames=False, with_file_content=False):
+    filenames = check_py_files(path, max_len=100)
+    trees = []
     for filename in filenames:
         with open(filename, 'r', encoding='utf-8') as attempt_handler:
             main_file_content = attempt_handler.read()
@@ -58,7 +63,11 @@ def get_verbs_from_function_name(function_name):
 
 def get_all_words_in_path(path):
     trees = [t for t in get_trees(path) if t]
-    function_names = [f for f in flat([get_all_names(t) for t in trees]) if not (f.startswith('__') and f.endswith('__'))]
+    function_names = [
+        f for f in flat([get_all_names(t) for t in trees])
+        if not (f.startswith('__') and f.endswith('__'))
+        ]
+
     def split_snake_case_name_to_words(name):
         return [n for n in name.split('_') if n]
     return flat([split_snake_case_name_to_words(function_name) for function_name in function_names])
@@ -68,13 +77,27 @@ def get_top_verbs_in_path(path, top_size=10):
     global Path
     Path = path
     trees = [t for t in get_trees(None) if t]
-    fncs = [f for f in flat([[node.name.lower() for node in ast.walk(t) if isinstance(node, ast.FunctionDef)] for t in trees]) if not (f.startswith('__') and f.endswith('__'))]
+    fncs = [
+        f for f in flat(
+            [[node.name.lower() for node in ast.walk(t) if isinstance(node, ast.FunctionDef)] for t in trees]
+            )
+        if not (f.startswith('__') and f.endswith('__'))
+        ]
+
     print('functions extracted')
     verbs = flat([get_verbs_from_function_name(function_name) for function_name in fncs])
     return collections.Counter(verbs).most_common(top_size)
+
+
 def get_top_functions_names_in_path(path, top_size=10):
     t = get_trees(path)
-    nms = [f for f in flat([[node.name.lower() for node in ast.walk(t) if isinstance(node, ast.FunctionDef)] for t in t]) if not (f.startswith('__') and f.endswith('__'))]
+    nms = [
+        f for f in flat(
+            [[node.name.lower() for node in ast.walk(t) if isinstance(node, ast.FunctionDef)] for t in t]
+            )
+        if not (f.startswith('__') and f.endswith('__'))
+        ]
+
     return collections.Counter(nms).most_common(top_size)
 
 
@@ -87,6 +110,8 @@ projects = [
     'requests',
     'sqlalchemy',
 ]
+
+
 for project in projects:
     path = os.path.join('.', project)
     wds += get_top_verbs_in_path(path)
